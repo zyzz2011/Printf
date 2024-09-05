29d088253aafbfc14a559e4f1cad99367dfc4002
zyzz2011 <169068710+zyzz2011@users.noreply.github.com>
2024-09-05 10:37:23 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+  public static void main(String[] args) {
+    String nome = "leonardo";
+    int dia = 9;
+    int mes = 12;
+    int ano = 2008;
+    double pi = 3.1459265359;
+    
+    System.out.printf("o valor de pi é: %.2f ",pi);
+
+    
+  }
+
+ 
+}
